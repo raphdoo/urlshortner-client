19136537e4bf7ce5f97873d1b3fcecf5bae9a2c6
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-06-19 20:00:35 +0100

    fixed dashboard bug

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -14,66 +14,68 @@ const Dashboard = () => {
           <Sidebar />
         </div>
 
-        <div className="col-12 col-md-10">
-          <h1 className="my-4">Dashboard</h1>
+        {user && (
+          <div className="col-12 col-md-10">
+            <h1 className="my-4">Dashboard</h1>
 
-          <Fragment>
-            <div className="row pr-4">
-              <div className="col-xl-12 col-sm-12 mb-3">
-                <div className="card text-white bg-primary o-hidden h-100">
-                  <div className="card-body">
-                    <div className="text-center card-font-size">
-                      Welcome
-                      <br /> <b>{user.user.email}</b>
+            <Fragment>
+              <div className="row pr-4">
+                <div className="col-xl-12 col-sm-12 mb-3">
+                  <div className="card text-white bg-primary o-hidden h-100">
+                    <div className="card-body">
+                      <div className="text-center card-font-size">
+                        Welcome
+                        <br /> <b>{user.user.email}</b>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="row pr-4">
-              <div className="col-xl-6 col-sm-6 mb-3">
-                <div className="card text-white bg-success o-hidden h-100">
-                  <div className="card-body">
-                    <div className="text-center card-font-size">
-                      Total Urls
-                      <br /> <b>{user.totalUrls}</b>
+              <div className="row pr-4">
+                <div className="col-xl-6 col-sm-6 mb-3">
+                  <div className="card text-white bg-success o-hidden h-100">
+                    <div className="card-body">
+                      <div className="text-center card-font-size">
+                        Total Urls
+                        <br /> <b>{user.totalUrls}</b>
+                      </div>
                     </div>
+                    <Link
+                      className="card-footer text-white clearfix small z-1"
+                      to="/urls"
+                    >
+                      <span className="float-left">View Details</span>
+                      <span className="float-right">
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
                   </div>
-                  <Link
-                    className="card-footer text-white clearfix small z-1"
-                    to="/urls"
-                  >
-                    <span className="float-left">View Details</span>
-                    <span className="float-right">
-                      <i className="fa fa-angle-right"></i>
-                    </span>
-                  </Link>
+                </div>
+
+                <div className="col-xl-6 col-sm-6 mb-3">
+                  <div className="card text-white bg-danger o-hidden h-100">
+                    <div className="card-body">
+                      <div className="text-center card-font-size">
+                        Total Clicks
+                        <br /> <b>{user.totalClicks}</b>
+                      </div>
+                    </div>
+                    <Link
+                      className="card-footer text-white clearfix small z-1"
+                      to="/analytics"
+                    >
+                      <span className="float-left">View Details</span>
+                      <span className="float-right">
+                        <i className="fa fa-angle-right"></i>
+                      </span>
+                    </Link>
+                  </div>
                 </div>
               </div>
-
-              <div className="col-xl-6 col-sm-6 mb-3">
-                <div className="card text-white bg-danger o-hidden h-100">
-                  <div className="card-body">
-                    <div className="text-center card-font-size">
-                      Total Clicks
-                      <br /> <b>{user.totalClicks}</b>
-                    </div>
-                  </div>
-                  <Link
-                    className="card-footer text-white clearfix small z-1"
-                    to="/analytics"
-                  >
-                    <span className="float-left">View Details</span>
-                    <span className="float-right">
-                      <i className="fa fa-angle-right"></i>
-                    </span>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </Fragment>
-        </div>
+            </Fragment>
+          </div>
+        )}
       </div>
     </Fragment>
   );
